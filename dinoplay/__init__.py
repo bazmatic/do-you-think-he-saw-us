3ecb457e1a8f9133d9d780d8f2148b6d272ff3c3
Barry Earsman <barry@earsman.com>
2026-04-28 14:05:37 +1000

    feat(pkg): export Settings, DinoEncoder, EmbeddingIndex, SearchHit

---
+++ dinoplay/__init__.py
@@ -0,0 +1,5 @@
+from dinoplay.config import Settings
+from dinoplay.index import EmbeddingIndex, SearchHit
+from dinoplay.model import DinoEncoder
+
+__all__ = ["Settings", "DinoEncoder", "EmbeddingIndex", "SearchHit"]
